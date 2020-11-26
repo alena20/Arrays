e7131bbf61e1b92ac0acf87759890a1bcc4f0dec
alena20 <lenanovik98@gmail.com>
2020-11-26 19:38:48 +0300

    create project

---
+++ src/main/java/edu/epam/jaggetarray/service/MaxNumService.java
@@ -0,0 +1,27 @@
+package edu.epam.jaggetarray.service;
+
+import edu.epam.jaggetarray.api.ICompare;
+
+
+
+public class MaxNumService implements ICompare {
+
+    @Override
+    public int compare(int[] firstArray, int[] secondArray) {
+        if (getLargestNumber(firstArray) > getLargestNumber(secondArray)) {
+            return 1;
+        } else{
+            return -1;
+        }
+    }
+
+    public int getLargestNumber(int[] array) {
+        int largestNumber = array[0];
+        for (Integer number : array) {
+            if (number > largestNumber) {
+                largestNumber = number;
+            }
+        }
+        return largestNumber;
+    }
+}
